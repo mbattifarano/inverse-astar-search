279ec8d4511b631751f715468d16cf9f936df105
Matt Battifarano <matthew.battifarano@gmail.com>
2020-04-10 14:03:50 -0400

    Use entropy in objective function; Refactor
    Previous experiments using only the sum of path costs in the
    objective resulted in infinite set of global optima. This was a
    direct consequence of normalizing the total cost over the out edges
    of each node (so that the edge costs relate to the transition
    probabilities out of each node). To mitigate this, jointly minimize
    the negative total out edge entropy over all nodes and the path cost.
    
    The constraints are the normalizing constraints on the out edges from
    each node, the constraint that all paths between the same OD pair are
    equal, and tha the cost of each discovered paths is at least that of
    the observed path(s) between the corresponding OD pair.
    paths are discovered by chaining compatible observed paths together.

--- src/inverse_astar_search/optim.py
+++ src/inverse_astar_search/optim.py
@@ -11,7 +11,7 @@ from cvxpy.expressions.expression import Expression
 
 import networkx as nx
 
-from typing import List, Iterable, Tuple, NamedTuple
+from typing import List, Iterable, Tuple, NamedTuple, Callable
 from itertools import product, starmap
 from toolz import curry, juxt, concat, compose
 from toolz.curried import map
@@ -19,29 +19,62 @@ from toolz.curried import map
 starmap = curry(starmap)
 
 
-def edge_cost(graph: nx.DiGraph) -> cp.Variable:
+def edge_cost(number_of_edges: int) -> cp.Variable:
     return cp.Variable(
-        graph.number_of_edges(),
-        nonneg=True
+        number_of_edges,
+        name="edge_cost",
+        nonneg=True,
     )
 
 
 def min_path_cost(number_of_trips: int) -> cp.Variable:
     return cp.Variable(
         number_of_trips,
+        name="min_trip_cost",
         nonneg=True
     )
 
+NodePairToVariable = Callable[[NodePair], cp.Variable]
 
-def edge_cost_is_normalized(edge_cost: cp.Variable) -> Constraint:
-    return cp.sum(edge_cost) == 1
+@curry
+def get_by_node_pair(index: NodePairIndex, variable: cp.Variable,
+                     node_pair: NodePair) -> cp.Varable:
+    return variable[index[node_pair]]
 
 
 @curry
-def path_cost(edge_index: NodePairIndex, edge_cost: cp.Variable, path: Path
-              ) -> Expression:
-    return cp.sum([edge_cost[edge_index[e]]
-                   for e in edges(path)])
+def successors(graph: nx.DiGraph, node: Node) -> Iterable[Node]:
+    return graph.successors(node)
+
+
+@curry
+def out_edges(successors: Callable[Node, Iterable[Node]],
+              node: Node) -> Iterable[NodePair]:
+    return (
+        (node, v)
+        for v in successors(node)
+    )
+
+
+def entropy(variables: Iterable[cp.Variable]) -> Expression:
+    return sum(map(cp.entr, variables))
+
+
+@curry
+def edges_entropy(edge_cost: NodePairToVariable, edges: Iterable[NodePair]) -> cp.Expression:
+    return entropy(map(edge_cost, edges))
+
+
+@curry
+def edge_cost_is_normalized(edge_cost: NodePairToVariable,
+                            edges: Iterable[NodePair]) -> Constraint:
+    return sum(map(edge_cost, edges)) == 1.0
+
+
+@curry
+def path_cost(edge_cost: NodePairToVariable, path: Path) -> Expression:
+    return sum(map(edge_cost, edges(path)))
+
 
 @curry
 def path_cost_gap(min_trip_cost: cp.Variable, path_cost: Expression
@@ -55,12 +88,6 @@ def gap_is_optimal(gap: Expression) -> Constraint:
 
 def gap_is_suboptimal(gap: Expression) -> Constraint:
     return gap >= 0.0
-
-
-@curry
-def min_trip_cost(trip_index: NodePairIndex, min_path_cost: cp.Variable,
-                  trip: NodePair) -> cp.Variable:
-    return min_path_cost[trip_index[trip]]
 
 
 @curry
@@ -86,36 +113,60 @@ def suboptimal_paths(nodes: Iterable[Node],
 
 
 def linear_program(graph: nx.DiGraph, paths: Paths) -> Result:
-    cost = edge_cost(graph)
+    cost = edge_cost(graph.number_of_edges())
 
     trips = list(set(map(trip, paths)))
     least_path_cost = min_path_cost(len(trips))
+    gamma = cp.Parameter(nonneg=True)
 
     edge_index = {e: i for i, e in enumerate(graph.edges)}
     trip_index = {t: i for i, t in enumerate(trips)}
 
+    edge_cost_of = get_by_node_pair(edge_index, cost)
+    trip_cost_of = get_by_node_pair(trip_index, least_path_cost)
+
+    out_edges_of_node = out_edges(successors(graph))  # Node -> Iterable[NodePair]
+
     min_trip_cost_from_path = compose(
-        min_trip_cost(trip_index, least_path_cost),
-        trip
-    )
+        trip_cost_of,  # NodePair -> cp.Variable
+        trip  # Path -> NodePair
+    )  # Path -> cp.Variable
 
     compute_gaps = compose(
         starmap(path_cost_gap),
         map(juxt(min_trip_cost_from_path,
-                 path_cost(edge_index, cost))
+                 path_cost(edge_cost_of))
             )
-    )
+    )  # Iterable[Path] -> Iterable[float]
 
-    constraints = [edge_cost_is_normalized(cost)]
+    out_edge_cost_is_normalized = compose(
+        edge_cost_is_normalized(edge_cost_of),
+        out_edges_of_node,
+    )  # Node -> Constraint
+
+    constraints = []
+
+    constraints.extend(map(out_edge_cost_is_normalized, graph.nodes))
+
     constraints.extend(map(gap_is_optimal, compute_gaps(paths)))
 
     other_paths = concat(map(suboptimal_paths(graph.nodes, trip_indexed_paths(paths)),
                              trips))
     other_paths = list(other_paths)
-    suboptimal_gaps = list(compute_gaps(other_paths))
+    suboptimal_gaps = compute_gaps(other_paths)
     constraints.extend(map(gap_is_suboptimal, suboptimal_gaps))
 
-    objective = cp.sum(suboptimal_gaps) if suboptimal_gaps else -cp.sum(least_path_cost)
+    out_edge_entropy = compose(
+        edges_entropy(edge_cost_of),  # Iterable[NodePair] -> cp.Expression
+        out_edges_of_node,  # Node -> Iterable[NodePair]
+    )  # Node -> cp.Expression
+
+    total_out_edge_entropy = compose(
+        sum,  # Iterable[cp.Expression] -> cp.Expression
+        map(out_edge_entropy) # Iterable[Node] -> Iterable[cp.Expression]
+    )  # Iterable[Node] -> cp.Expression
+
+    objective = total_out_edge_entropy(graph.nodes) - cp.sum(least_path_cost)
 
     problem = cp.Problem(
         cp.Maximize(objective),
@@ -124,6 +175,7 @@ def linear_program(graph: nx.DiGraph, paths: Paths) -> Result:
 
     return Result(
         problem=problem,
+        penalty=gamma,
         edge_cost=cost,
         min_trip_cost=least_path_cost,
         edge_index=edge_index,
@@ -134,6 +186,7 @@ def linear_program(graph: nx.DiGraph, paths: Paths) -> Result:
 
 class Result(NamedTuple):
     problem: cp.Problem
+    penalty: cp.Parameter
     edge_cost: cp.Variable
     min_trip_cost: cp.Variable
     edge_index: NodePairIndex
