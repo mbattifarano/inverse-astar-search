04eb640ca1f27e1d395f30ed823275b6011fa4e8
Pablo <pabloguarda@cmu.edu>
2020-05-01 14:44:29 -0400

    Removed unuseful test
    Signed-off-by: Pablo <pabloguarda@cmu.edu>

--- test/test_network.py
+++ test/test_network.py
@@ -6,8 +6,3 @@ from inverse_astar_search.network import create_network
 import networkx as nx
 import matplotlib.pyplot as plt
 
-def test_show_multiDiNetwork():
-    A = get_adjacency_toynetwork(1)
-    W = random_edge_weights(A=A, limits=(1, 3))
-    show_multiDiNetwork(G0 = create_network(W), show_labels = False)
-
